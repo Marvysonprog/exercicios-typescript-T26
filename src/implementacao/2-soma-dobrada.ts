b4faff1ead058ae63d523dee22b2f6dc0b069f95
mylenaverspeelt <mylena49@hotmail.com>
2024-09-04 15:59:17 -0300

    feat: Resposta do Exercicio2

--- src/implementacao/2-soma-dobrada.ts
+++ src/implementacao/2-soma-dobrada.ts
@@ -1,4 +1,14 @@
-function somaDobrada(umValor: number, outroValor: number) {
-    return null;
+function somaDobrada(umValor: number, outroValor: number): number {
+
+    if (umValor === outroValor) {
+        const soma = umValor + outroValor;
+        const dobroDaSoma = soma * 2;
+        return dobroDaSoma;
+    } else if (umValor < 1 || outroValor < 1) {
+        return -1
+    }else{
+        const soma = umValor + outroValor;
+        return soma;
+    }
 }
 module.exports = somaDobrada;

78c4c6ef9316ecca54bad863aeb3647571483e70
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-05 11:31:26 -0300

    feat: Resposta do Exercicio5

--- src/implementacao/5-objeto-data.ts
+++ src/implementacao/5-objeto-data.ts
@@ -1,4 +1,23 @@
-function criaObjeto(data: string) {
-    return null;
+function criaObjeto(data: string): Object {
+    let dia : string
+    let mes : string
+    let ano : string 
+
+    dia = data.slice(0,2)
+    mes = data.slice(3,5)
+    ano = data.slice(6,10)
+
+    let calendario : {dia: string, mes: string, ano: string} = {
+        dia: "",
+        mes: "",
+        ano: ""
+    };
+    
+    calendario.dia = dia;
+    calendario.mes = mes;
+    calendario.ano = ano;
+
+    return calendario;
+
 }
 module.exports = criaObjeto;

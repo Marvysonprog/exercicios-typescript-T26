18e56bf139405542f120c24803d7f7af8f7580a5
mylenaverspeelt <mylena49@hotmail.com>
2024-09-05 14:41:24 -0300

    feat: Resposta do Exercicio7

--- src/implementacao/7-numeros-armstrong.ts
+++ src/implementacao/7-numeros-armstrong.ts
@@ -1,4 +1,25 @@
 function numerosArmstrong(numero: number) {
-    return null;
+    let arrayCaracteres: number[] = [];
+
+    let stringDoNumero = numero.toString();
+  
+    for (let i = 0; i < stringDoNumero.length; i++) {
+      let numeroConvertido = parseInt(stringDoNumero[i]);
+      arrayCaracteres.push(numeroConvertido);
+    }
+  
+    let quantidadeDeCaracteres = arrayCaracteres.length;
+  
+    let somaDosvalores = 0;
+  
+    arrayCaracteres.forEach(function (item) {
+      somaDosvalores += item ** quantidadeDeCaracteres;
+    });
+  
+    if (somaDosvalores == numero) {
+      return "Este eh um numero de Armstrong!";
+    } else {
+      return "Este nao eh um numero de Armstrong!";
+    }
 }
 module.exports = numerosArmstrong;

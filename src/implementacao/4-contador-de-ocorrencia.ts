3b4238213754d9396f0a2b8e1b219be0cb847490
mylenaverspeelt <mylena49@hotmail.com>
2024-09-05 10:30:48 -0300

    feat: Resposta do Exercicio4

--- src/implementacao/4-contador-de-ocorrencia.ts
+++ src/implementacao/4-contador-de-ocorrencia.ts
@@ -1,4 +1,20 @@
-function contadorDeOcorrencia(texto: string, letra: string) {
-    return null;
+function contadorDeOcorrencia(texto: string, busca: string):number {
+    
+    let arrayTexto : string[] = [];
+
+    for (let i = 0; i < texto.length; i++) {
+        arrayTexto.push(texto[i]);
+    }
+
+    let contador = 0;
+
+    for (let j = 0; j < arrayTexto.length; j++) {
+    
+        if (arrayTexto[j] == busca) {
+            contador++;
+        }
+    }
+    return contador;
+
 }
 module.exports = contadorDeOcorrencia;
